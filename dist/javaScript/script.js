a493c43f174bb6053da6e1f967c26ec03df51c2d
NicoleArriagada <nicole.magdalena@gmail.com>
2022-03-25 18:09:39 -0300

    Primer commit

---
+++ dist/javaScript/script.js
@@ -0,0 +1,7 @@
+//Calls the nav & footer to a  <div id="name-placeholder"></div> into a html file. ( also uses a Jquery)
+$(function(){
+    $("#nav-placeholder").load("nav.html")
+  })
+  $(function(){
+    $("#footer-placeholder").load("footer.html")
+  })
